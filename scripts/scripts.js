b1784fdd804e4b9ed91660c6a66b7620e6b98251
mjvalade <mjvalade@gmail.com>
2016-09-29 22:16:42 -0600

    weeks 1 and 4 complete

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,12 @@
-var $ = require('./jquery');
+// var $ = require('./jquery');
 
-// $('.tab-link').on('click', function () {
-//
-// })
+// Week 1: Nav bar mobile menu
+function mobileMenu() {
+    var x = document.getElementById("navList");
+    if (x.className === "nav-list") {
+        x.className += " responsive";
+    }
+    else {
+        x.className = "nav-list";
+    }
+}
